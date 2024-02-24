89be528f05def28a69e8bfe42f30fbf682758e56
LeonardoCaa <lleonardocamargost@gmail.com>
2024-02-23 21:59:25 -0300

    janela de splash finalizada

--- ProjetoABC/frmSplash.cs
+++ ProjetoABC/frmSplash.cs
@@ -16,5 +16,15 @@ namespace ProjetoABC
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
