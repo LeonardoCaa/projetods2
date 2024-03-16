253c34489bb9d3daedf533aa0438bbc94ee8d12e
LeonardoCaa <lleonardocamargost@gmail.com>
2024-03-15 22:12:11 -0300

    finalizando tela de funcionarios

--- ProjetoABC/frmFuncionarios.Designer.cs
+++ ProjetoABC/frmFuncionarios.Designer.cs
@@ -47,6 +47,20 @@ namespace ProjetoABC
             this.btnCadastrar = new System.Windows.Forms.Button();
             this.btnExcluir = new System.Windows.Forms.Button();
             this.btnNovo = new System.Windows.Forms.Button();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.maskedTextBox4 = new System.Windows.Forms.MaskedTextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
             this.pnlFuncionarios.SuspendLayout();
             this.gpbDadosPessoais.SuspendLayout();
             this.SuspendLayout();
@@ -64,10 +78,24 @@ namespace ProjetoABC
             this.pnlFuncionarios.Location = new System.Drawing.Point(-4, 479);
             this.pnlFuncionarios.Name = "pnlFuncionarios";
             this.pnlFuncionarios.Size = new System.Drawing.Size(791, 84);
-            this.pnlFuncionarios.TabIndex = 1;
+            this.pnlFuncionarios.TabIndex = 12;
             // 
             // gpbDadosPessoais
             // 
+            this.gpbDadosPessoais.Controls.Add(this.label13);
+            this.gpbDadosPessoais.Controls.Add(this.label10);
+            this.gpbDadosPessoais.Controls.Add(this.comboBox1);
+            this.gpbDadosPessoais.Controls.Add(this.label9);
+            this.gpbDadosPessoais.Controls.Add(this.textBox7);
+            this.gpbDadosPessoais.Controls.Add(this.textBox6);
+            this.gpbDadosPessoais.Controls.Add(this.textBox5);
+            this.gpbDadosPessoais.Controls.Add(this.maskedTextBox4);
+            this.gpbDadosPessoais.Controls.Add(this.label8);
+            this.gpbDadosPessoais.Controls.Add(this.label7);
+            this.gpbDadosPessoais.Controls.Add(this.label6);
+            this.gpbDadosPessoais.Controls.Add(this.textBox4);
+            this.gpbDadosPessoais.Controls.Add(this.label5);
+            this.gpbDadosPessoais.Controls.Add(this.maskedTextBox2);
             this.gpbDadosPessoais.Controls.Add(this.label4);
             this.gpbDadosPessoais.Controls.Add(this.maskedTextBox1);
             this.gpbDadosPessoais.Controls.Add(this.textBox3);
@@ -79,8 +107,8 @@ namespace ProjetoABC
             this.gpbDadosPessoais.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.gpbDadosPessoais.Location = new System.Drawing.Point(12, 34);
             this.gpbDadosPessoais.Name = "gpbDadosPessoais";
-            this.gpbDadosPessoais.Size = new System.Drawing.Size(760, 212);
-            this.gpbDadosPessoais.TabIndex = 2;
+            this.gpbDadosPessoais.Size = new System.Drawing.Size(760, 315);
+            this.gpbDadosPessoais.TabIndex = 0;
             this.gpbDadosPessoais.TabStop = false;
             this.gpbDadosPessoais.Text = "Dados Pessoais";
             // 
@@ -99,14 +127,14 @@ namespace ProjetoABC
             this.maskedTextBox1.Mask = "999,999,999-00";
             this.maskedTextBox1.Name = "maskedTextBox1";
             this.maskedTextBox1.Size = new System.Drawing.Size(142, 24);
-            this.maskedTextBox1.TabIndex = 6;
+            this.maskedTextBox1.TabIndex = 4;
             // 
             // textBox3
             // 
             this.textBox3.Location = new System.Drawing.Point(466, 57);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(287, 24);
-            this.textBox3.TabIndex = 5;
+            this.textBox3.TabIndex = 3;
             // 
             // label3
             // 
@@ -122,7 +150,7 @@ namespace ProjetoABC
             this.textBox2.Location = new System.Drawing.Point(76, 57);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(325, 24);
-            this.textBox2.TabIndex = 3;
+            this.textBox2.TabIndex = 2;
             // 
             // label2
             // 
@@ -156,7 +184,7 @@ namespace ProjetoABC
             this.btnVoltar.Location = new System.Drawing.Point(16, 37);
             this.btnVoltar.Name = "btnVoltar";
             this.btnVoltar.Size = new System.Drawing.Size(101, 33);
-            this.btnVoltar.TabIndex = 5;
+            this.btnVoltar.TabIndex = 13;
             this.btnVoltar.Text = "&Novo";
             this.btnVoltar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnVoltar.UseVisualStyleBackColor = true;
@@ -168,7 +196,7 @@ namespace ProjetoABC
             this.btnLimpar.Location = new System.Drawing.Point(561, 38);
             this.btnLimpar.Name = "btnLimpar";
             this.btnLimpar.Size = new System.Drawing.Size(101, 33);
-            this.btnLimpar.TabIndex = 4;
+            this.btnLimpar.TabIndex = 18;
             this.btnLimpar.Text = "&Limpar";
             this.btnLimpar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnLimpar.UseVisualStyleBackColor = true;
@@ -180,7 +208,7 @@ namespace ProjetoABC
             this.btnPesquisar.Location = new System.Drawing.Point(454, 38);
             this.btnPesquisar.Name = "btnPesquisar";
             this.btnPesquisar.Size = new System.Drawing.Size(101, 33);
-            this.btnPesquisar.TabIndex = 3;
+            this.btnPesquisar.TabIndex = 17;
             this.btnPesquisar.Text = "&Pesquisar";
             this.btnPesquisar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnPesquisar.UseVisualStyleBackColor = true;
@@ -192,7 +220,7 @@ namespace ProjetoABC
             this.btnAlterar.Location = new System.Drawing.Point(240, 38);
             this.btnAlterar.Name = "btnAlterar";
             this.btnAlterar.Size = new System.Drawing.Size(101, 33);
-            this.btnAlterar.TabIndex = 2;
+            this.btnAlterar.TabIndex = 15;
             this.btnAlterar.Text = "&Alterar";
             this.btnAlterar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnAlterar.UseVisualStyleBackColor = true;
@@ -204,7 +232,7 @@ namespace ProjetoABC
             this.btnCadastrar.Location = new System.Drawing.Point(133, 38);
             this.btnCadastrar.Name = "btnCadastrar";
             this.btnCadastrar.Size = new System.Drawing.Size(101, 33);
-            this.btnCadastrar.TabIndex = 1;
+            this.btnCadastrar.TabIndex = 14;
             this.btnCadastrar.Text = "&Cadastrar";
             this.btnCadastrar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnCadastrar.UseVisualStyleBackColor = true;
@@ -216,7 +244,7 @@ namespace ProjetoABC
             this.btnExcluir.Location = new System.Drawing.Point(347, 38);
             this.btnExcluir.Name = "btnExcluir";
             this.btnExcluir.Size = new System.Drawing.Size(101, 33);
-            this.btnExcluir.TabIndex = 1;
+            this.btnExcluir.TabIndex = 16;
             this.btnExcluir.Text = "&Excluir";
             this.btnExcluir.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnExcluir.UseVisualStyleBackColor = true;
@@ -233,6 +261,122 @@ namespace ProjetoABC
             this.btnNovo.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnNovo.UseVisualStyleBackColor = true;
             this.btnNovo.Click += new System.EventHandler(this.btnVoltar_Click);
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(352, 110);
+            this.maskedTextBox2.Mask = "9999-9999";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(80, 24);
+            this.maskedTextBox2.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(289, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "&Celular";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(76, 163);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(325, 24);
+            this.textBox4.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(-2, 166);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 18);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "&Endereço";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(422, 166);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 18);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "&Número";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(604, 166);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(39, 18);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "&CEP";
+            // 
+            // maskedTextBox4
+            // 
+            this.maskedTextBox4.Location = new System.Drawing.Point(664, 163);
+            this.maskedTextBox4.Mask = "99999-999";
+            this.maskedTextBox4.Name = "maskedTextBox4";
+            this.maskedTextBox4.Size = new System.Drawing.Size(80, 24);
+            this.maskedTextBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(482, 163);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 24);
+            this.textBox5.TabIndex = 7;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(76, 227);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(179, 24);
+            this.textBox6.TabIndex = 9;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(331, 227);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(179, 24);
+            this.textBox7.TabIndex = 10;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(15, 230);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(48, 18);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "&Bairro";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ItemHeight = 18;
+            this.comboBox1.Location = new System.Drawing.Point(618, 227);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(53, 26);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(557, 230);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(55, 18);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "&Estado";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(277, 230);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(54, 18);
+            this.label13.TabIndex = 23;
+            this.label13.Text = "&Cidade";
             // 
             // frmFuncionarios
             // 
@@ -273,5 +417,19 @@ namespace ProjetoABC
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
     }
 }
